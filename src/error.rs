336c6ee2fb8bff6eeb2f98895a37bfd0283f7675
Kalle Kankaanpää <unset>
2022-06-21 23:12:37 +0300

    got format_win_error working properly

--- src/error.rs
+++ src/error.rs
@@ -2,12 +2,14 @@ use std::ptr;
 use thiserror::Error;
 use windows::Win32::{
     Foundation::WIN32_ERROR,
-    System::Diagnostics::Debug::{
-        FormatMessageW, FORMAT_MESSAGE_ALLOCATE_BUFFER, FORMAT_MESSAGE_FROM_SYSTEM,
+    System::{
+        Diagnostics::Debug::{
+            FormatMessageW, FORMAT_MESSAGE_ALLOCATE_BUFFER, FORMAT_MESSAGE_FROM_SYSTEM,
+            FORMAT_MESSAGE_IGNORE_INSERTS,
+        },
+        Memory::LocalFree,
     },
 };
-
-use windows::core::PWSTR;
 
 #[derive(Error, Debug)]
 pub enum RegistryError {
@@ -20,23 +22,35 @@ pub enum RegistryError {
 pub fn format_win_error(error: WIN32_ERROR) -> Option<String> {
     if error.is_err() {
         unsafe {
-            println!("{}", error.0);
-            let output = PWSTR::default();
+            let mut buffer: *mut u16 = ptr::null_mut();
+            let lp_buffer = &mut buffer as *mut *mut u16 as *mut u16;
             let output_len = FormatMessageW(
-                FORMAT_MESSAGE_ALLOCATE_BUFFER & FORMAT_MESSAGE_FROM_SYSTEM,
-                ptr::null(),
+                FORMAT_MESSAGE_ALLOCATE_BUFFER
+                    | FORMAT_MESSAGE_FROM_SYSTEM
+                    | FORMAT_MESSAGE_IGNORE_INSERTS,
+                ptr::null_mut(),
                 error.0,
                 0,
-                output,
+                std::mem::transmute(lp_buffer),
                 0,
-                ptr::null(),
+                ptr::null_mut(),
             );
-            let output_buffer = std::slice::from_raw_parts(output.0, output_len as _);
-            println!("{:?}", output_buffer);
+            let output_buffer = std::slice::from_raw_parts(buffer, output_len as _);
+            LocalFree(std::mem::transmute(buffer));
 
             Some(String::from_utf16_lossy(output_buffer))
         }
     } else {
         None
     }
+}
+
+#[test]
+fn test_win_error_formatting() {
+    let error = format_win_error(WIN32_ERROR(5));
+    assert!(error.is_some());
+    println!("{:?}", error);
+    let error = format_win_error(WIN32_ERROR(6));
+    assert!(error.is_some());
+    println!("{:?}", error);
 }
